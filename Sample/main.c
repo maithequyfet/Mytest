8f98ec92d7ca4dc06aafbfe841cbeb9f40bcba58
maithequyfet <maithequy@gmail.com>
2017-06-23 11:38:48 +0700

    message

--- Sample/main.c
+++ Sample/main.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include "DegitDefine.h"
 
-char gFirstNumber[100] = "222233.3211";
-char gSecondNumber[100] = "3393.624";
+char gFirstNumber[100] = "222232222222222222223.3212002201";
+char gSecondNumber[100] = "93393.624";
 
 /* Function to dectect the location of character '.' */
-int GetPointInfo(char *s){ 
+int GetPointInfo(char *s){
 	int i = 0;
 	for(i = 0; i < strlen(s); i++){
 		if(s[i] == '.'){
@@ -17,6 +18,7 @@ int GetPointInfo(char *s){
 	return i;
 }
 
+/* Covert a single letter to the correspond digit */
 int CovertStringToDigit(char s){
 	int temp;
     switch(s){
@@ -55,6 +57,8 @@ int CovertStringToDigit(char s){
     }
 	return temp;
 }
+
+/* Covert a digit to the correspond letter */
 char CovertDigitToString(int num){
 	char temp;
     switch(num){
@@ -103,7 +107,7 @@ char AddBeforePoint(int point1, int point2,char *s){
 	i = point1 - 1;
 	j = point2 - 1;
 	s[0] = 49;    /* ASCII of zero */
-	
+
 	for (i;i >= 0; i--){
 		if(j >= 0){
 			total = CovertStringToDigit(gFirstNumber[i]) + CovertStringToDigit(gSecondNumber[j])+ surplus;
@@ -123,11 +127,12 @@ char AddBeforePoint(int point1, int point2,char *s){
 		total = 0;
 	    j--;
 	}
-	
+
 	s[0] = CovertDigitToString(surplus);
 	return *s;
 }
 
+/* Function to add fractional part of two number */
 char AddAfterPoint(int point1, int point2,char *s){
     int i = 0;
 	int j = 0;
@@ -138,10 +143,10 @@ char AddAfterPoint(int point1, int point2,char *s){
 	temp = point1 + strlen(gSecondNumber) - point2 - 1;
 	s[point1 + 1] = 46;									/* ASCII of '.' */
 	s[point1 + 2] = 49; 							/* ASCII of zero */     							/* ASCII of '.' */
-	
+
 	i = strlen(gFirstNumber) - 1;
 	j = strlen(gSecondNumber) - 1;
-	
+
 	for(i; i > point1; i--){
 		if(i > temp){
 			total = CovertStringToDigit(gFirstNumber[i]);
@@ -150,7 +155,7 @@ char AddAfterPoint(int point1, int point2,char *s){
 			total = CovertStringToDigit(gFirstNumber[i]) + CovertStringToDigit(gSecondNumber[j])+ surplus;
 			j--;
 		}
-		
+
 		if(total >= 10){
 			surplus = 1;
 			total = total % 10;
@@ -163,43 +168,47 @@ char AddAfterPoint(int point1, int point2,char *s){
 		total = 0;
 	}
 	s[point1 + 2] = CovertDigitToString(surplus);
-	
+
 	return *s;
 }
 
+/* Remove zero letter at head of string and before the point */
 void RemoveZeroLetter(char *s){
 	int i = 0;
 	int len = strlen(s);
-	int point = GetPointInfo(s);
-	if(s[i] = 49){
+	int point = 0;
+	if(s[0] == 48){
 	    for(i = 0; i < len; i++){
 		    s[i] = s[i+1];
 		}
 		s[len - 1] = NULL;
+		len = len - 1;
 	}
-	
-	len = len - 1; 
-	
-	if(s[point] = 49){
-		for(i = 0; i < len; i++){
+
+    point = GetPointInfo(s);
+	if(s[point + 1] == 48){
+		for(i = point + 1; i < len; i++){
 		    s[i] = s[i+1];
 		}
 		s[len - 1] = NULL;
 	}
-	
+
 }
 
 int main(){
 	char *s;
-	int i = 0;
 	int point1 = GetPointInfo(gFirstNumber);
 	int point2 = GetPointInfo(gSecondNumber);
+
 	s = (char*)malloc(sizeof(s)*100);
+
 	*s = AddBeforePoint(point1,point2,s);
-	printf("%s\n",s);
 	*s = AddAfterPoint(point1,point2,s);
+
+	printf("%s\n",s);
 	RemoveZeroLetter(s);
 	printf("%s\n",s);
+
 	free(s);
 	return 0;
 }
